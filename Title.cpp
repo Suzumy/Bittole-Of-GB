e9c058aaed4d01bbf98b86a91a4565b1c9a8e824
Suzumy <ryouga.0085@gmail.com>
2020-04-20 01:20:24 +0900

    タイトル画面の完成

--- Title.cpp
+++ Title.cpp
@@ -3,7 +3,7 @@
 #include "Title.h"
 
 //画像用変数
-int machine;
+int machine, gametitle;
 
 //フラグ
 int imgloadflag = 0;
@@ -14,20 +14,33 @@ void TitleUpdate()
 		imgloadflag = 1;
 		LoadImg_Title();
 	}
+
+	//次の画面への遷移
+	if (CheckHitKey(KEY_INPUT_SPACE) != 0) {
+
+	}
 }
 
 void TitleDraw()
 {
+	//画像の表示
 	DrawGraph(0, 0, machine, TRUE);
+	DrawGraph(200, 100, gametitle, TRUE);
+
+	//文字列の表示
+	DrawString(200, 300, "スペースキーでスタート", GetColor(0, 0, 0));
+
 }
 
 void LoadImg_Title()
 {
 	machine = LoadGraph("img/machine.png");
+	gametitle = LoadGraph("img/gametitle.png");
 }
 
 void ReleaseImg_Title()
 {
 	DeleteGraph(machine);
+	DeleteGraph(gametitle);
 	imgloadflag = 0;
 }
